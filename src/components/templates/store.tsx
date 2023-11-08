1a2b94abc64674804bb18977869c5e12536571a7
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-11-08 16:04:04 +0500

    Fixing Store.tsx

--- src/components/templates/store.tsx
+++ src/components/templates/store.tsx
@@ -24,46 +24,47 @@ function Store({id, name, price, imgUrl }: StoreItemProps)  {
 const quantity = getItemQuantity(id);
 
   return (
-    <div>
-       {storeitems.map(item => (
-      <Col key={item.id}>
-        <Card className='card-wrapper h-100 rounded-5'>
-            <div className='card-image--wrapper'>
-                <Card.Img className='card-img h-100' variant = "top" src={item.imgUrl} style={{ objectFit: "cover", objectPosition: "center" }}/>
-            </div>
-        <Card.Body className='d-flex flex-column'>
-            <Card.Title className='d-flex justify-content-between align-items-center mb-4'>
-                <span className='fs-2 card-title'>{item.name}</span>
-                {/* <span className='ms-2 text-muted'>{formatCurrency(item.price)}</span> */}
-            </Card.Title>
+    <h1>Store</h1>
+    // <div>
+    //    {storeitems.map(item => (
+    //   <Col key={item.id}>
+    //     <Card className='card-wrapper h-100 rounded-5'>
+    //         <div className='card-image--wrapper'>
+    //             <Card.Img className='card-img h-100' variant = "top" src={item.imgUrl} style={{ objectFit: "cover", objectPosition: "center" }}/>
+    //         </div>
+    //     <Card.Body className='d-flex flex-column'>
+    //         <Card.Title className='d-flex justify-content-between align-items-center mb-4'>
+    //             <span className='fs-2 card-title'>{item.name}</span>
+    //             {/* <span className='ms-2 text-muted'>{formatCurrency(item.price)}</span> */}
+    //         </Card.Title>
             
-            {/* <div className='mt-mt-auto'>
-                {quantity === 0 ? (
-                    <Button className='card-button w-100' onClick={() => increaseCartQuantity(id)}>
-                        + Add to Cart
-                    </Button>
-                ): (
-                    <div className='d-flex align-items-center flex-column' style={{ gap: ".5rem" }}>
-                        <div  className='d-flex align-items-center justify-content-center' style={{ gap: ".5rem" }}>
-                            <Button onClick={() => decreaseCartQuantity(id)}>
-                                -
-                            </Button>
-                            <div>
-                                <span className='fs-3'>{quantity}</span> in cart
-                            </div>
-                            <Button onClick={() => increaseCartQuantity(id)}>+</Button>
-                        </div>
-                        <Button onClick={() => removeFromCart(id)} variant="danger" size="sm">
-                            Remove
-                        </Button>
-                    </div>
-                ) }
-            </div> */}
-        </Card.Body>
-        </Card> 
-      </Col>
-        ))}
-    </div>
+    //         {/* <div className='mt-mt-auto'>
+    //             {quantity === 0 ? (
+    //                 <Button className='card-button w-100' onClick={() => increaseCartQuantity(id)}>
+    //                     + Add to Cart
+    //                 </Button>
+    //             ): (
+    //                 <div className='d-flex align-items-center flex-column' style={{ gap: ".5rem" }}>
+    //                     <div  className='d-flex align-items-center justify-content-center' style={{ gap: ".5rem" }}>
+    //                         <Button onClick={() => decreaseCartQuantity(id)}>
+    //                             -
+    //                         </Button>
+    //                         <div>
+    //                             <span className='fs-3'>{quantity}</span> in cart
+    //                         </div>
+    //                         <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+    //                     </div>
+    //                     <Button onClick={() => removeFromCart(id)} variant="danger" size="sm">
+    //                         Remove
+    //                     </Button>
+    //                 </div>
+    //             ) }
+    //         </div> */}
+    //     </Card.Body>
+    //     </Card> 
+    //   </Col>
+    //     ))}
+    // </div>
    
   )
 }
